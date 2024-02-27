8202028fc74ece42bfb1030165961d319b79e652
youness <guadir2022@gmail.com>
2024-02-27 15:31:08 +0100

    its my first commit

---
+++ src/Marcher/MacherStor/MarcherAppStor.jsx
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import MarcherApp from "../MarcherApp";
+import StorMarcher from "./MarcherStor";
+
+export default function MarcherAppStor() {
+    return <>
+    
+    <Provider store={StorMarcher}>
+        <MarcherApp/>
+    </Provider>
+    </>
+}
